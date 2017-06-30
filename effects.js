36d86c7ae5dfdcdfe992563dac55b8d9d5918197
viclai <victor1.lai@gmail.com>
2017-06-29 21:19:18 -0700

    Align Images in Projects

--- effects.js
+++ effects.js
@@ -12,7 +12,8 @@ $(document).ready(function(){
     var hash = this.hash;
 
     // Using jQuery's animate() method to add smooth page scroll
-    // The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
+    // The optional number (900) specifies the number of milliseconds it takes
+    // to scroll to the specified area
     $('html, body').animate({
       scrollTop: $(hash).offset().top
     }, 900, function(){
@@ -21,5 +22,26 @@ $(document).ready(function(){
       window.location.hash = hash;
       });
     } // End if
+  });
+
+  $(window).scroll(function() {
+    $(".slideanim").each(function(){
+      var pos = $(this).offset().top;
+
+      var winTop = $(window).scrollTop();
+      if (pos < winTop + 600) {
+        $(this).addClass("slide");
+      }
+    });
+  });
+
+  $('.pull-down-half').each(function() {
+    var $this = $(this);
+    $this.css('margin-top', 0.5 * ($this.parent().height() - $this.height()))
+  });
+
+  $('.pull-down').each(function() {
+    var $this = $(this);
+    $this.css('margin-top', $this.parent().height() - $this.height())
   });
 })
