92d3c4605d1aba9888caf40ff4143b912d8ba15f
viclai <victor1.lai@gmail.com>
2018-01-07 12:43:04 -0800

    Redesign Website
    * Migrate to Bootstrap 4.
    
    * Use Bootstrap cards for projects to make more web-responsive.

--- effects.js
+++ effects.js
@@ -1,30 +1,18 @@
-$(document).ready(function(){
-  // Add smooth scrolling to all links in navbar + footer link
-  $(".navbar a, footer a[href='#home']").on('click', function(event) {
-
-   // Make sure this.hash has a value before overriding default behavior
-  if (this.hash !== "") {
-
-    // Prevent default anchor click behavior
-    event.preventDefault();
-
-    // Store hash
-    var hash = this.hash;
-
-    // Using jQuery's animate() method to add smooth page scroll
-    // The optional number (900) specifies the number of milliseconds it takes
-    // to scroll to the specified area
-    $('html, body').animate({
-      scrollTop: $(hash).offset().top
-    }, 900, function(){
-
-      // Add hash (#) to URL when done scrolling (default click behavior)
-      window.location.hash = hash;
-      });
-    } // End if
-  });
+$(document).ready(function() {
+  var offset = $('#about').offset();
+  var scroll_start = 0;
 
   $(window).scroll(function() {
+    scroll_start = $(this).scrollTop();
+
+    if (scroll_start > offset.top) {
+      $('.navbar').css('background-color', '#a2dafa');
+      $('.navbar').css('opacity', '0.95');
+    } else {
+      $('.navbar').css('background-color', 'transparent');
+      $('.navbar').css('opacity', 'inherit');
+    }
+
     $(".slideanim").each(function(){
       var pos = $(this).offset().top;
 
