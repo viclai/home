a8ce41f2101b92196f14a98506204d2b316b28bd
viclai <victor1.lai@gmail.com>
2019-11-03 10:32:41 -0800

    Convert to React App
    * Use `create-react-app` to create initial files for a React
    application.

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          This website is currently under construction.
+        </p>
+        <p>
+        Please contact Victor if you wish to collaborate with him on a project or get a copy of his resume.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
