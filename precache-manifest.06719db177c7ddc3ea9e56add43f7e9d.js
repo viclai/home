a307668a89aa76bc7c53b8cc005b344156a2b480
viclai <victor1.lai@gmail.com>
2019-11-03 10:26:14 -0800

    Updates

---
+++ precache-manifest.06719db177c7ddc3ea9e56add43f7e9d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7570be2a1b6b9a7c0fa6616d3e1b1dca",
+    "url": "/victor-main/index.html"
+  },
+  {
+    "revision": "37ed4b18116416fedc7c",
+    "url": "/victor-main/static/css/main.6e4f5b06.chunk.css"
+  },
+  {
+    "revision": "21b566eec6bb675462fc",
+    "url": "/victor-main/static/js/2.2a5ed353.chunk.js"
+  },
+  {
+    "revision": "37ed4b18116416fedc7c",
+    "url": "/victor-main/static/js/main.5561e9e5.chunk.js"
+  },
+  {
+    "revision": "f0f050b53f50a57cf1ad",
+    "url": "/victor-main/static/js/runtime-main.bab68891.js"
+  }
+]);
